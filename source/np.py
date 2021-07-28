da8ee61a2c49331577e8ddbf2180cc32177f9241
sontis3 <oleg.ovcharenko@labworks.ru>
2021-07-28 13:33:25 +0300

    +np.py

---
+++ source/np.py
@@ -0,0 +1,14 @@
+import numpy
+a = numpy.arange(5)
+a.dtype
+a.shape
+a.dtype.itemsize
+m = numpy.array([numpy.arange(2), numpy.arange(2)])
+
+# %%
+import numpy as np
+
+# %%
+x = np.array([[1,2,3],[4,5,6]])
+x
+# %%
